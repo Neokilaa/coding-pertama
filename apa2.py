879d25b2e8aa2bd695e2f07c6c7ab3a588a770d5
kila <kaylaaadewiii23@gmail.com>
2024-07-07 00:50:50 +0700

    codingan kedua

---
+++ apa2.py
@@ -0,0 +1,33 @@
+user_name = "joanne"
+user_id = 18
+verified = True
+
+magnum = "matcha"
+skin_care = "cosrx"
+fav_number = 23
+fav_2_number = "11"
+# hasil = 23+"11" 
+# print(hasil)
+benar = True
+salah = False
+print(verified)
+print(salah)
+pi = 3.14
+print(pi)
+print(user_name)
+user_name = "kaylaaa"
+print(user_name)
+user_id = 23 #gue ganti id nih
+print(user_id)
+penggaris = "cosmos kayak \
+    monyet"
+print(penggaris)
+user_id = -23
+print(user_id)
+pertama = "hai "
+kedua = "rana "
+ketiga = "kentank"
+gabung = pertama+kedua+ketiga
+print(gabung)
+gabungan = gabung+"!"
+print(gabungan)
---
+++ error.py
@@ -0,0 +1,14 @@
+print("Hello, world!"
+# kurang adanya tutup kurung
+
+if x > 10
+    print("x is greater than 10")
+# seharusnya setelah sepuluh ada ":"
+
+y = 5 + 
+
+# seharusnya ada angka lagi
+
+result = 10 + = 5
+
+# harusnya "=" tidak perlu
