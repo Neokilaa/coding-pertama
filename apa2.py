3dd40cc2ff3088234ef1aaf3d4dae1cc465ad84a
kila <kaylaaadewiii23@gmail.com>
2024-07-07 10:14:08 +0700

    pr dari zakin

--- apa2.py
+++ apa2.py
@@ -1,29 +1,42 @@
+# string, nama
 user_name = "joanne"
+# integers, user id
 user_id = 18
+# boolean
 verified = True
 
+# string, rasa magnum favorit
 magnum = "matcha"
+# string, skincare favorit
 skin_care = "cosrx"
+# integers, nomor favorit
 fav_number = 23
+# string, nomor favorit 2
 fav_2_number = "11"
 # hasil = 23+"11" 
 # print(hasil)
+# boolean
 benar = True
 salah = False
 print(verified)
 print(salah)
+# floating number / angka yg tidak bulat
 pi = 3.14
+# mencetak floating number 
 print(pi)
+# pengubahan variabel nama dan id
 print(user_name)
 user_name = "kaylaaa"
 print(user_name)
 user_id = 23 #gue ganti id nih
 print(user_id)
+# kalimat dapat dibagi barisnya menggunakan backslash \
 penggaris = "cosmos kayak \
     monyet"
 print(penggaris)
 user_id = -23
 print(user_id)
+# penggabungan beberapa variabel menggunakan tanda +
 pertama = "hai "
 kedua = "rana "
 ketiga = "kentank"
